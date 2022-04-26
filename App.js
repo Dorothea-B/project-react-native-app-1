338dda59707beafe68c35c81b6bc9beb7186241f
Dorothea-B <barre.dorothea@gmail.com>
2022-04-26 21:54:39 +0200

    set up dependencies

--- App.js
+++ App.js
@@ -3,20 +3,21 @@ import styled from 'styled-components/native';
 
 const Container = styled.View`
 	flex: 1;
-	background-color: papayawhip;
+	background-color: lavender;
 	justify-content: center;
 	align-items: center;
 `;
 
 const Title = styled.Text`
-	font-size: 24px;
-	color: palevioletred;
+	font-size: 35px;
+	color: black;
+	text-align: center;
 `;
 
 const App = () => {
 	return (
 		<Container>
-			<Title>This is your cool app!</Title>
+			<Title>This is Dorothea's super cool app!</Title>
 			<Title>Go to App.js and start coding</Title>
 			<Title>💅💅💅</Title>
 		</Container>
